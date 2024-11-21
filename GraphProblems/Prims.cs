d52828ea2f1a3fea7de818b218d72b150f0aa9b1
matishen7 <ishenbaev7@gmail.com>
2024-11-21 16:48:29 -0600

    MinCostConnectPoints

--- GraphProblems/Prims.cs
+++ GraphProblems/Prims.cs
@@ -61,5 +61,65 @@ namespace Neetcode150.GraphProblems
 
             return (visited.Count == n) ? total : -1;
         }
+
+        public static int MinCostConnectPoints(int[][] points)
+        {
+            Dictionary<int, List<(int node, int cost)>> adjList = new();
+            for (int i = 0; i < points.Length; i++)
+            {
+                adjList[i] = new List<(int node, int cost)>();
+            }
+
+            for (int i = 0; i < points.Length; i++)
+                for (int j = 0; j < points.Length; j++)
+                {
+                    if (i == j) continue;
+                    int w = CalculateCost(points, i, j);
+                    if (!adjList[i].Contains((j,w))) adjList[i].Add((j, w));
+                    if (!adjList[j].Contains((i, w))) adjList[j].Add((i, w));
+                }
+
+            HashSet<int> visited = new();
+            var mst = new HashSet<(int, int)>();
+            var pq = new PriorityQueue<(int w, int src, int dst), int>();
+            int total = 0;
+            foreach (var neighbor in adjList[0])
+            {
+                var node = neighbor.node;
+                var weight = neighbor.cost;
+                pq.Enqueue((weight, 0, node), weight);
+            }
+
+            visited.Add(0);
+
+            while (pq.Count > 0)
+            {
+                var curr = pq.Dequeue();
+                var w = curr.w; int src = curr.src; int dst = curr.dst;
+
+                if (visited.Contains(dst)) continue;
+                mst.Add((src, dst));
+                total += w;
+                visited.Add(dst);
+                foreach (var neighbor in adjList[dst])
+                {
+                    var node = neighbor.node;
+                    var weight = neighbor.cost;
+                    if (!visited.Contains(node))
+                        pq.Enqueue((weight, dst, node), weight);
+                }
+
+            };
+
+            return total;
+        }
+
+        private static int CalculateCost(int[][] points, int src, int dst)
+        {
+            var coordinates1 = points[src];
+            var coordinates2 = points[dst];
+            int cost = Math.Abs(coordinates1[0] - coordinates2[0]) + Math.Abs(coordinates1[1] - coordinates2[1]);
+            return cost;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,12 +5,11 @@ using Neetcode150.ObjectOriented.ConnectFour;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
 using static Neetcode150.TreeProblems;
-int n = 5;
-int[][] flights = [[4, 1, 1], [1, 2, 3], [0, 3, 2], [0, 4, 10], [3, 1, 1], [1, 4, 3]];
-int src = 2;
-int dst = 1;
-int k = 1;
+
+int[][] points = [[0, 0], [2, 2], [3, 3], [2, 4], [4, 2]];
 
 
 
-AdvancedGraphs.FindCheapestPrice(n, flights, src, dst, k);
+
+
+Prims.MinCostConnectPoints(points);
