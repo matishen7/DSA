2f952966e96f2410845667d885009fc64573086d
matishen7 <ishenbaev7@gmail.com>
2024-11-22 17:41:47 -0600

    foreignDictionary

--- Program.cs
+++ Program.cs
@@ -6,13 +6,9 @@ using System.Security.Cryptography;
 using System.Threading.Tasks;
 using static Neetcode150.TreeProblems;
 
-var tickets = new List<List<string>>
-        {
-              new List<string> { "BUF", "HOU" },
-            new List<string> { "HOU", "SEA" },
-            new List<string> { "JFK", "BUF" } };
+string[] dic = ["hrn", "hrf", "er", "enn", "rfnn"];
 
 
 
 
-Prims.FindItinerary(tickets);
+TopologicalSort.foreignDictionary(dic);
--- TopologicalSort.cs
+++ TopologicalSort.cs
@@ -52,5 +52,86 @@ namespace Neetcode150
             topSort.Add(src);
             return true;
         }
+
+        public static string foreignDictionary(string[] words)
+        {
+            Dictionary<char, HashSet<char>> adj = new Dictionary<char, HashSet<char>>();
+            foreach (var word in words)
+            {
+                foreach (var c in word)
+                {
+                    if (!adj.ContainsKey(c))
+                    {
+                        adj[c] = new HashSet<char>();
+                    }
+                }
+            }
+            for (int i = 0; i < words.Length - 1; i++)
+            {
+                var w1 = words[i];
+                var w2 = words[i + 1];
+                int minLen = Math.Min(w1.Length, w2.Length);
+                if (w1.Length > w2.Length && w1.Substring(0, minLen) == w2.Substring(0, minLen))
+                {
+                    return "";
+                }
+                for (int j = 0; j < minLen; j++)
+                {
+                    if (w1[j] != w2[j])
+                    {
+                        adj[w1[j]].Add(w2[j]);
+                        break;
+                    }
+                }
+            }
+            StringBuilder topSort = new StringBuilder();
+            HashSet<char> visit = new HashSet<char>();
+            HashSet<char> path = new HashSet<char>();
+            foreach (var pair in adj.Keys)
+            {
+                if (!foreignDictionaryHelper(pair, adj, visit, path, topSort)) return string.Empty;
+            }
+
+            var annswer = new StringBuilder();
+            for (int i = topSort.Length - 1; i >= 0; i--)
+            {
+                annswer.Append(topSort[i]);
+            }
+
+            return annswer.ToString();
+        }
+
+        public static bool foreignDictionaryHelper(char cc, Dictionary<char, HashSet<char>> adj, HashSet<char> visit, HashSet<char> path, StringBuilder topSort)
+        {
+            if (visit.Contains(cc))
+                return true;
+
+            if (path.Contains(cc))
+                return false;
+
+            path.Add(cc);
+            if (adj.ContainsKey(cc))
+                foreach (var nei in adj[cc])
+                    if (!foreignDictionaryHelper(nei, adj, visit, path, topSort)) return false;
+
+            path.Remove(cc);
+            visit.Add(cc);
+
+            topSort.Append(cc);
+            return true;
+        }
+
+        private static void get(string first, string second, Dictionary<char, char> dic)
+        {
+            int i = 0;
+
+            while (i < first.Length && i < second.Length)
+            {
+                if (!dic.ContainsKey(second[i]) && first[i] != second[i])
+                    dic.Add(second[i], first[i]);
+                i++;
+            }
+            return;
+        }
     }
 }
