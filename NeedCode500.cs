284833c01e9d4a7abb0127775e501d9637463d12
matishen7 <ishenbaev7@gmail.com>
2024-12-17 14:06:39 -0600

    MinimumTotal

---
+++ NeedCode500.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neetcode150
+{
+    internal class NeedCode500
+    {
+        public static int SpecialArray(int[] nums)
+        {
+            for (int i = 0; i <= nums.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[j] >= i) count++;
+                }
+
+                if (count == i) return count;
+            }
+
+            return -1;
+        }
+
+        public static int ArrangeCoins(int n)
+        {
+            if (n == 1) return 0;
+            int row = 1;
+            while (n > row)
+            {
+                n = n - row;
+                row++;
+            }
+
+            return row - 1;
+        }
+
+        public static bool IsAlienSorted(string[] words, string order)
+        {
+            for (int i = 0; i < words.Length - 1; i++)
+            {
+                var word1 = words[i];
+                var word2 = words[i + 1];
+                int k = 0;
+                while (k < Math.Min(word1.Length, word2.Length) && word1[k] == word2[k]) k++;
+                if (k == word2.Length && word1.Length > k) return false;
+                for (int m = k; m < word1.Length; m++)
+                {
+                    char w1 = word1[m];
+                    char w2 = word2[m];
+                    var index1 = order.IndexOf(w1);
+                    var index2 = order.IndexOf(w2);
+                    if (index1 < index2) break;
+                    else return false;
+                }
+
+            }
+
+            return true;
+        }
+
+        public static int MinimumTotal(List<List<int>> triangle)
+        {
+            var memo = new Dictionary<(int, int), int>();
+            int answer = MinimumTotalHelper(0, 0, triangle, memo);
+            return answer;
+        }
+
+        public static int MinimumTotalHelper(int i, int j, List<List<int>> triangle, Dictionary<(int, int), int> memo)
+        {
+            if (i >= triangle.Count) return 0;
+
+            if (memo.ContainsKey((i, j))) return memo[(i, j)];
+
+            int first = triangle[i][j] + MinimumTotalHelper(i + 1, j, triangle, memo);
+            int second = int.MaxValue;
+            if (j < i)
+             second = triangle[i][j + 1] + MinimumTotalHelper(i + 1, j + 1, triangle, memo);
+
+            memo[(i,j)] = Math.Min(first, second);
+            return memo[(i, j)];
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,18 +8,10 @@ using static Neetcode150.Intervals;
 using static Neetcode150.MathAndGeometry;
 using static Neetcode150.TreeProblems;
 
-int[] arr = { 9,9,9 };
-
-
-CountSquares countSquares = new CountSquares();
-countSquares.Add([1, 1]);
-countSquares.Add([2, 2]);
-countSquares.Add([1, 2]);
-
-Console.WriteLine(countSquares.Count([2, 1]));   // return 1.
-Console.WriteLine(countSquares.Count([3, 3]));   // return 0.
-countSquares.Add([2, 2]);     // Duplicate points are allowed.
-Console.WriteLine(countSquares.Count([2, 1]));   // return 2. 
-
-
-//Console.WriteLine(MathAndGeometry.SetZeroes(matrix));
+var list = new List<List<int>>()
+{
+    new List<int>(){-1},
+    new List<int>(){2,3},
+    new List<int>(){1,-1,-3},
+};
+NeedCode500.MinimumTotal(list);
