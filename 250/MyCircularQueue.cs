a5ed2e874fc4b8742aa311f1be1a93830e74e390
matishen7 <ishenbaev7@gmail.com>
2025-01-13 13:57:06 -0600

    MyCircularQueue

---
+++ 250/MyCircularQueue.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neetcode150._250
+{
+
+    public class MyCircularQueue
+    {
+        private Node root = null;
+        private int k;
+        private int count = 0;
+        public MyCircularQueue(int k)
+        {
+            this.k = k;
+        }
+
+        public bool EnQueue(int value)
+        {
+            var newNode = new Node(value);
+            if (root == null)
+            {
+                root = newNode;
+                count++;
+                return true;
+            }
+                
+            Node? current = root;
+            
+            if (count < k)
+            {
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+                count++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool DeQueue()
+        {
+            if (root == null) { return false; }
+
+            root = root.Next;
+            count--;
+            return true;
+            
+
+        }
+
+        public int Front()
+        {
+            return (root == null) ? -1 : root.Value;
+        }
+
+        public int Rear()
+        {
+            if (root == null) return -1;
+
+            Node node = root;
+            while (node.Next != null)
+                node = node.Next;
+            return node.Value;
+        }
+
+        public bool IsEmpty()
+        {
+            return (root == null);
+        }
+
+        public bool IsFull()
+        {
+            return count == k;
+        }
+    }
+
+    /**
+     * Your MyCircularQueue object will be instantiated and called as such:
+     * MyCircularQueue obj = new MyCircularQueue(k);
+     * bool param_1 = obj.EnQueue(value);
+     * bool param_2 = obj.DeQueue();
+     * int param_3 = obj.Front();
+     * int param_4 = obj.Rear();
+     * bool param_5 = obj.IsEmpty();
+     * bool param_6 = obj.IsFull();
+     */
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,15 @@
-﻿using Neetcode150;
-using static Neetcode150.NeedCode500;
-FreqStack freqStack = new FreqStack();
-freqStack.Push(5); // The stack is [5]
-freqStack.Push(7); // The stack is [5,7]
-freqStack.Push(5); // The stack is [5,7,5]
-freqStack.Push(7); // The stack is [5,7,5,7]
-freqStack.Push(4); // The stack is [5,7,5,7,4]
-freqStack.Push(5); // The stack is [5,7,5,7,4,5]
-Console.WriteLine(freqStack.Pop());   // return 5, as 5 is the most frequent. The stack becomes [5,7,5,7,4].
-Console.WriteLine(freqStack.Pop());   // return 7, as 5 and 7 is the most frequent, but 7 is closest to the top. The stack becomes [5,7,5,4].
-Console.WriteLine(freqStack.Pop());   // return 5, as 5 is the most frequent. The stack becomes [5,7,4].
-Console.WriteLine(freqStack.Pop());   // return 4, as 4, 5 and 7 is the most frequent, but 4 is closest to the top. The stack becomes [5,7].
+﻿using Neetcode150._250;
 
-
+MyCircularQueue myCircularQueue = new MyCircularQueue(3);
+myCircularQueue.EnQueue(1);
+myCircularQueue.EnQueue(2);
+myCircularQueue.EnQueue(3);
+myCircularQueue.EnQueue(4);
+Console.WriteLine(myCircularQueue.Rear());     // return 3
+Console.WriteLine(myCircularQueue.IsFull());   // return True
+Console.WriteLine(myCircularQueue.DeQueue());  // return True
+Console.WriteLine(myCircularQueue.DeQueue());  // return True
+Console.WriteLine(myCircularQueue.DeQueue());  // return True
+Console.WriteLine(myCircularQueue.DeQueue());  // return True
+Console.WriteLine(myCircularQueue.EnQueue(4)); // return True
+Console.WriteLine(myCircularQueue.Rear());
