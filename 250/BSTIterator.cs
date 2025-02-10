ea39f3c3015d51dd53eccc4c74f3dcfb43e824bd
matishen7 <ishenbaev7@gmail.com>
2025-02-10 14:51:38 -0600

    BSTIterator

---
+++ 250/BSTIterator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Neetcode150.TreeProblems;
+
+namespace Neetcode150._250
+{
+    public class BSTIterator
+    {
+        private Stack<TreeNode> stack = new Stack<TreeNode>();
+
+        public BSTIterator(TreeNode root)
+        {
+            TreeNode curr = root;
+
+            while (curr != null)
+            {
+
+                stack.Push(curr);
+                curr = curr.left;
+            }
+        }
+
+        public int Next()
+        {
+            var curr = stack.Pop();
+
+            if (curr.right != null)
+            {
+                var temp = curr.right;
+                while (temp != null)
+                {
+                    stack.Push(temp);
+                    temp = temp.left;
+                }
+            }
+
+            return curr.val;
+        }
+
+        public bool HasNext()
+        {
+            return stack.Count > 0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,4 +3,4 @@ using Neetcode150._250;
 using Neetcode150.GraphProblems;
 using Neetcode150.ObjectOriented.ConnectFour;
 string[] deadends = ["0201", "0101", "0102", "1212", "2002"]; string target = "0202";
-Graphs.OpenLock(deadends, target);
+BSTIterator.RemoveDuplicates("deeedbbcccbdaa", 3);
