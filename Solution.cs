933f0da47d0e65bfc3f64ed16e097f228ad6c9ec
matishen7 <ishenbaev7@gmail.com>
2025-02-13 15:33:45 -0600

    SplitArray

--- Solution.cs
+++ Solution.cs
@@ -105,6 +105,114 @@ namespace Neetcode150
 
             return days;
         }
+
+        public static bool Search(int[] nums, int target)
+        {
+            var pivot = FindPivot(nums);
+            int left = 0;
+            int right = pivot;
+            int result = BinarySearch(nums, target, left, right);
+            if (result != -1)
+            {
+                return true;
+            }
+
+            left = pivot + 1;
+            right = nums.Length - 1;
+            result = BinarySearch(nums, target, left, right);
+            if (result != -1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static int BinarySearch(int[] nums, int target, int left, int right)
+        {
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+                else if (nums[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return -1;
+        }
+
+        private static int FindPivot(int[] nums)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] > nums[i + 1])
+                {
+                    return i;
+                }
+            }
+
+            return nums.Length - 1;
+        }
+
+        public static int SplitArray(int[] nums, int k)
+        {
+            int sum = 0;
+            int max = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                max = Math.Max(nums[i], max);
+                sum += nums[i];
+            }
+
+            int left = max, right = sum, result = sum;
+
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                int posK = CalculateSplits(mid, nums);
+                if (posK <= k)
+                {
+                    result = Math.Min(result, mid);
+                    right = mid;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            return result;
+
+        }
+
+        private static int CalculateSplits(int p, int[] nums)
+        {
+            int days = 1;
+            int sum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (sum + nums[i] <= p)
+                {
+                    sum += nums[i];
+                }
+                else
+                {
+                    days++;
+                    sum = nums[i];
+                }
+            }
+
+            return days;
+        }
     }
 
 
