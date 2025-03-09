8e22adedb96e72ab5d302d55dc2b1c2de85b0fe9
matishen7 <ishenbaev7@gmail.com>
2025-03-09 17:26:39 -0500

    LongestCommonPrefix

--- Meta.cs
+++ Meta.cs
@@ -13,6 +13,21 @@ namespace Neetcode150
 
     public class Meta
     {
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            var prefix = strs[0];
+            for (int i = 1; i < strs.Length; i++)
+            {
+                while (strs[i].IndexOf(prefix) != 0)
+                {
+                    prefix = prefix.Substring(0, prefix.Length - 1);
+                }
+
+                if (prefix.Length == 0) return "";
+            }
+
+            return prefix;
+        }
         public static string AddStrings(string num1, string num2)
         {
             var sb1 = new StringBuilder(num1);
