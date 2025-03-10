71ab780864537f1cf07ce218221acd4e2abea5e4
matishen7 <ishenbaev7@gmail.com>
2025-03-10 15:27:31 -0500

    SolutionMarch

---
+++ LeetcodeMarch/SolutionMarch.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Neetcode150.TreeProblems;
+
+namespace Neetcode150.LeetcodeMarch
+{
+    internal class SolutionMarch
+    {
+        public IList<int> RightSideView(TreeNode root)
+        {
+            if (root == null) return new List<int>();
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            var answer = new List<int>();
+            while (queue.Count > 0)
+            {
+                int levelLength = queue.Count;
+                for (int i = 0; i < levelLength; i++)
+                {
+                    var curr = queue.Dequeue();
+                    if (i == levelLength - 1)
+                        answer.Add(curr.val);
+                   
+                    if (curr.left != null)
+                        queue.Enqueue(curr.left);
+                    if (curr.right != null)
+                        queue.Enqueue(curr.right);
+
+                }
+            }
+            return answer;
+        }
+        public static string MergeAlternately(string word1, string word2)
+        {
+            var sb = new StringBuilder();
+            int i = 0;
+            while (i < word1.Length && i < word2.Length)
+            {
+                sb.Append(word1[i]);
+                sb.Append(word2[i]);
+                i++;
+            }
+
+            while (i < word1.Length)
+            {
+                sb.Append(word1[i]);
+                i++;
+            }
+            while (i < word2.Length)
+            {
+                sb.Append(word2[i]);
+                i++;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ MetaQuestions/MedianFinder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neetcode150.MetaQuestions
+{
+    public class MedianFinder
+    {
+        PriorityQueue<int, int> min = new PriorityQueue<int, int>();
+        PriorityQueue<int, int> max = new PriorityQueue<int, int>();
+
+        public MedianFinder()
+        {
+
+        }
+
+        public void AddNum(int num)
+        {
+            max.Enqueue(num, -num);
+            
+            //balancing
+            var val = max.Peek();
+            min.Enqueue(val, val);
+            max.Dequeue();
+
+            if (max.Count < min.Count)
+            {
+                max.Enqueue(min.Peek(), -min.Peek());
+                min.Dequeue();
+            }
+
+        }
+
+        public double FindMedian()
+        {
+            return (max.Count > min.Count) ? max.Peek(): ((double) max.Peek() + (double)min.Peek())*0.5;
+        }
+    }
+}
