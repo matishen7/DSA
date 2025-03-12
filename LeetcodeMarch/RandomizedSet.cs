85101c93370391604e3f8fa849417388e24f89d2
matishen7 <ishenbaev7@gmail.com>
2025-03-12 14:27:10 -0500

    RandomizedSet

---
+++ LeetcodeMarch/RandomizedSet.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neetcode150.LeetcodeMarch
+{
+    public class RandomizedSet
+    {
+        Dictionary<int, int> dict;
+        List<int> list;
+
+        public RandomizedSet()
+        {
+            dict = new Dictionary<int, int>();
+            list = new List<int>();
+        }
+
+        public bool Insert(int val)
+        {
+            if (!dict.ContainsKey(val))
+            {
+                dict.Add(val, list.Count);
+                list.Add(val);
+                return true;
+            }
+            return false;
+        }
+
+        public bool Remove(int val)
+        {
+            if (dict.ContainsKey(val))
+            {
+                var index = dict[val];
+                var last = list[list.Count - 1];
+                
+                list[index] = last;
+                dict[last] = index;
+                dict.Remove(val);
+                var count = list.Count - 1;
+                list.RemoveAt(count);
+                return true;
+            }
+            return false;
+        }
+
+        public int GetRandom()
+        {
+            var random = new Random();
+            var index = random.Next(0, list.Count);
+            return list[index];
+        }
+    }
+
+    /**
+     * Your RandomizedSet object will be instantiated and called as such:
+     * RandomizedSet obj = new RandomizedSet();
+     * bool param_1 = obj.Insert(val);
+     * bool param_2 = obj.Remove(val);
+     * int param_3 = obj.GetRandom();
+     */
+}
--- Program.cs
+++ Program.cs
@@ -10,5 +10,12 @@ using Node = Neetcode150.Meta.Node;
 //int?[] arr = [5, 5, 5, 5, 5, null, 5];
 //var root = TreeProblems.BuildTree(arr);
 //TreeProblems.PrintBST(root);
-
-AmazonSolutions.HasSameDigits("3902");
+RandomizedSet randomizedSet = new RandomizedSet();
+randomizedSet.Insert(1); // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+randomizedSet.Remove(2); // Returns false as 2 does not exist in the set.
+randomizedSet.Insert(2); // Inserts 2 to the set, returns true. Set now contains [1,2].
+randomizedSet.GetRandom(); // getRandom() should return either 1 or 2 randomly.
+randomizedSet.Remove(1); // Removes 1 from the set, returns true. Set now contains [2].
+randomizedSet.Insert(2); // 2 was already in the set, so return false.
+randomizedSet.GetRandom(); // Since 2 is the only number in the set, getRandom() will always return 2.
+//AmazonSolutions.HasSameDigits("3902");
