9d2ff56e5d9e996001fda196c739f4fb8d6ee764
matishen7 <ishenbaev7@gmail.com>
2025-03-12 11:51:37 -0500

    SQL

---
+++ LeetcodeMarch/SQL.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neetcode150.LeetcodeMarch
+{
+    public class SQL
+    {
+        private Dictionary<string, Dictionary<int, List<string>>> tables;
+        private Dictionary<string, (int colNum, int lastCol)> colNums;
+        public SQL(IList<string> names, IList<int> columns)
+        {
+            tables = new Dictionary<string, Dictionary<int, List<string>>>();
+            colNums = new Dictionary<string, (int colNum, int lastCol)>();
+            for (int i = 0; i < names.Count; i++)
+            {
+                string name = names[i];
+                int col = columns[i];
+                colNums.Add(name, (col, 1));
+                var values = new Dictionary<int, List<string>>();
+                tables.Add(name, values);
+            }
+        }
+
+        public bool Ins(string name, IList<string> row)
+        {
+            if (tables.ContainsKey(name))
+            {
+                var lastColumn = colNums[name];
+                if (lastColumn.colNum != row.Count) return false;
+                var rows = tables[name];
+                var newRow = new List<string>();
+                for (int i = 0; i < row.Count; i++)
+                {
+                    newRow.Add(row[i]);
+                }
+                rows.Add(lastColumn.lastCol, newRow);
+                tables[name] = rows;
+                lastColumn.lastCol += 1;
+                colNums[name] = lastColumn;
+                return true;
+            }
+            else return false;
+        }
+
+        public void Rmv(string name, int rowId)
+        {
+            if (!tables.ContainsKey(name)) return;
+            if (!tables[name].ContainsKey(rowId)) return;
+            tables[name].Remove(rowId);
+
+        }
+
+        public string Sel(string name, int rowId, int columnId)
+        {
+            if (!tables.ContainsKey(name)) return null;
+            if (!tables[name].ContainsKey(rowId)) return null;
+            var row = tables[name][rowId];
+            if (row.Count < columnId) return "<null>"; return row[columnId - 1];
+        }
+
+        public IList<string> Exp(string name)
+        {
+            if (!tables.ContainsKey(name)) return new List<string>() { "<null>"};
+
+            var values = new List<string>();
+
+            foreach (var row in tables[name])
+            {
+                var id = row.Key.ToString();
+                var cols = row.Value.Select(col => col.Trim('"')); // Remove extra quotes
+
+                // Join ID and columns into a single comma-separated string
+                values.Add($"{id},{string.Join(",", cols)}");
+            }
+
+            return values;
+
+        }
+    }
+
+    /**
+     * Your SQL object will be instantiated and called as such:
+     * SQL obj = new SQL(names, columns);
+     * bool param_1 = obj.Ins(name,row);
+     * obj.Rmv(name,rowId);
+     * string param_3 = obj.Sel(name,rowId,columnId);
+     * IList<string> param_4 = obj.Exp(name);
+     */
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,25 @@ using Node = Neetcode150.Meta.Node;
 //int?[] arr = [5, 5, 5, 5, 5, null, 5];
 //var root = TreeProblems.BuildTree(arr);
 //TreeProblems.PrintBST(root);
-int[][] matrix = [[1, 1, 1], [1, 0, 1], [1, 1, 1]];
-int target = -5;
-Console.WriteLine(AmazonSolutions.SetZeroes(matrix));
+// Creates three tables.
+SQL sQL = new SQL(["one", "two", "three"], [2, 3, 1]);
+
+// Adds a row to the table "two" with id 1. Returns True. 
+sQL.Ins("two", ["first", "second", "third"]);
+
+// Returns the value "third" from the third column 
+// in the row with id 1 of the table "two".
+sQL.Sel("two", 1, 3);
+
+// Removes the first row of the table "two".
+sQL.Rmv("two", 1);
+
+// Returns "<null>" as the cell with id 1 
+// has been removed from table "two".
+sQL.Sel("two", 1, 2);
+
+// Returns False as number of columns are not correct.
+sQL.Ins("two", ["fourth", "fifth"]);
+
+// Adds a row to the table "two" with id 2. Returns True.
+sQL.Ins("two", ["fourth", "fifth", "sixth"]);
