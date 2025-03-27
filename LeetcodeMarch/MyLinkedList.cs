7f621671581e348434fa3a546934c98695c1e446
matishen7 <ishenbaev7@gmail.com>
2025-03-27 14:36:18 -0500

    MyLinkedList

---
+++ LeetcodeMarch/MyLinkedList.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Neetcode150.LinkedListProblems;
+
+namespace Neetcode150.LeetcodeMarch
+{
+    public class MyLinkedList
+    {
+        private ListNode? head;
+        private ListNode? tail;
+        private int count;
+        public MyLinkedList()
+        {
+            head = null;
+            tail = head;
+            count = -1;
+        }
+
+        public int Get(int index)
+        {
+            if (index >= count) return -1;
+
+            int i = 0;
+            var current = head;
+            while (i < count && current.next != null)
+            {
+                current = current.next;
+                i++;
+            }
+            return current.val;
+        }
+
+        public void AddAtHead(int val)
+        {
+            var node = new ListNode(val);
+            node.next = head;
+            head = node;
+            count += 1;
+        }
+
+        public void AddAtTail(int val)
+        {
+            var node = new ListNode(val);
+            var curr = head;
+            while (curr.next != null)
+            {
+                curr = curr.next;
+            }
+            curr.next = node;
+            count += 1;
+        }
+
+        public void AddAtIndex(int index, int val)
+        {
+            if (index >= count) return;
+            var node = new ListNode(val);
+            int i = 0;
+            var prev = head;
+            var current = head;
+            while (i < count && current.next != null)
+            {
+                prev = current;
+                current = current.next;
+                i++;
+            }
+
+            prev.next = node;
+            node.next = current;
+            count += 1;
+        }
+
+        public void DeleteAtIndex(int index)
+        {
+            if (index >= count) return;
+            int i = 0;
+            var prev = head;
+            var current = head;
+            while (i < count && current.next != null)
+            {
+                prev = current;
+                current = current.next;
+                i++;
+            }
+
+            prev.next = current.next;
+            count -= 1;
+        }
+    }
+
+    /**
+     * Your MyLinkedList object will be instantiated and called as such:
+     * MyLinkedList obj = new MyLinkedList();
+     * int param_1 = obj.Get(index);
+     * obj.AddAtHead(val);
+     * obj.AddAtTail(val);
+     * obj.AddAtIndex(index,val);
+     * obj.DeleteAtIndex(index);
+     */
+}
--- Program.cs
+++ Program.cs
@@ -21,14 +21,12 @@ using Node = Neetcode150.Meta.Node;
 //Console.WriteLine(SolutionMarch.SwapNodes(node1, 2));
 
 
-BrowserHistory browserHistory = new BrowserHistory("leetcode.com");
-browserHistory.Visit("google.com");       // You are in "leetcode.com". Visit "google.com"
-browserHistory.Visit("facebook.com");     // You are in "google.com". Visit "facebook.com"
-browserHistory.Visit("youtube.com");      // You are in "facebook.com". Visit "youtube.com"
-browserHistory.Back(1);                   // You are in "youtube.com", move back to "facebook.com" return "facebook.com"
-browserHistory.Back(1);                   // You are in "facebook.com", move back to "google.com" return "google.com"
-browserHistory.Forward(1);                // You are in "google.com", move forward to "facebook.com" return "facebook.com"
-browserHistory.Visit("linkedin.com");     // You are in "facebook.com". Visit "linkedin.com"
-browserHistory.Forward(2);                // You are in "linkedin.com", you cannot move forward any steps.
-browserHistory.Back(2);                   // You are in "linkedin.com", move back two steps to "facebook.com" then to "google.com". return "google.com"
-browserHistory.Back(7);                   // You are in "google.com", you can move back only one step to "leetcode.com". return "leetcode.com"
+MyLinkedList myLinkedList = new MyLinkedList();
+myLinkedList.AddAtHead(1);
+myLinkedList.AddAtHead(2);
+myLinkedList.AddAtTail(3);
+myLinkedList.AddAtTail(4);
+myLinkedList.AddAtIndex(1, 2);    // linked list becomes 1->2->3
+myLinkedList.Get(1);              // return 2
+myLinkedList.DeleteAtIndex(1);    // now the linked list is 1->3
+myLinkedList.Get(1);                   // You are in "google.com", you can move back only one step to "leetcode.com". return "leetcode.com"
