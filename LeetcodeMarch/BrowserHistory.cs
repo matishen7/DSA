d9baf14db2e7fcf9b19d9d9bb2b29c0e571bc846
matishen7 <ishenbaev7@gmail.com>
2025-03-27 13:51:01 -0500

    BrowserHistory

---
+++ LeetcodeMarch/BrowserHistory.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neetcode150.LeetcodeMarch
+{
+    public class BrowserHistory
+    {
+        private DoubleLinkedList head;
+        private DoubleLinkedList current;
+
+        public BrowserHistory(string homepage)
+        {
+            head = new DoubleLinkedList(homepage);
+            current = head;
+        }
+
+        public void Visit(string url)
+        {
+            var prev = current;
+            var node = new DoubleLinkedList(url);
+            current.next = node;
+            current = current.next;
+            current.prev = prev;
+        }
+
+        public string Back(int steps)
+        {
+            int i = 0;
+            while (i < steps && current.prev != null)
+            {
+                i++;
+                current = current.prev;
+            }
+
+            return current.url;
+        }
+
+        public string Forward(int steps)
+        {
+            int i = 0;
+            while (i < steps && current.next != null)
+            {
+                i++;
+                current = current.next;
+            }
+
+            return current.url;
+        }
+
+        public class DoubleLinkedList
+        {
+            public string url = string.Empty;
+            public DoubleLinkedList? prev = null;
+            public DoubleLinkedList? next = null;
+
+            public DoubleLinkedList(string url)
+            {
+                this.url = url;
+            }
+
+            public DoubleLinkedList(string url, DoubleLinkedList? next, DoubleLinkedList? prev)
+            {
+                this.url = url;
+                this.prev = prev;
+                this.next = next;
+            }
+        }
+    }
+
+   
+
+    /**
+     * Your BrowserHistory object will be instantiated and called as such:
+     * BrowserHistory obj = new BrowserHistory(homepage);
+     * obj.Visit(url);
+     * string param_2 = obj.Back(steps);
+     * string param_3 = obj.Forward(steps);
+     */
+}
--- Program.cs
+++ Program.cs
@@ -15,7 +15,20 @@ using Node = Neetcode150.Meta.Node;
 //var node5 = new ListNode(5);
 //var node4 = new ListNode(4, node5);
 //var node3 = new ListNode(3, node4);
-var node2 = new ListNode(2);
-var node1 = new ListNode(1, node2);
+//var node2 = new ListNode(2);
+//var node1 = new ListNode(1, node2);
 
-Console.WriteLine(SolutionMarch.SwapNodes(node1, 2));
+//Console.WriteLine(SolutionMarch.SwapNodes(node1, 2));
+
+
+BrowserHistory browserHistory = new BrowserHistory("leetcode.com");
+browserHistory.Visit("google.com");       // You are in "leetcode.com". Visit "google.com"
+browserHistory.Visit("facebook.com");     // You are in "google.com". Visit "facebook.com"
+browserHistory.Visit("youtube.com");      // You are in "facebook.com". Visit "youtube.com"
+browserHistory.Back(1);                   // You are in "youtube.com", move back to "facebook.com" return "facebook.com"
+browserHistory.Back(1);                   // You are in "facebook.com", move back to "google.com" return "google.com"
+browserHistory.Forward(1);                // You are in "google.com", move forward to "facebook.com" return "facebook.com"
+browserHistory.Visit("linkedin.com");     // You are in "facebook.com". Visit "linkedin.com"
+browserHistory.Forward(2);                // You are in "linkedin.com", you cannot move forward any steps.
+browserHistory.Back(2);                   // You are in "linkedin.com", move back two steps to "facebook.com" then to "google.com". return "google.com"
+browserHistory.Back(7);                   // You are in "google.com", you can move back only one step to "leetcode.com". return "leetcode.com"
