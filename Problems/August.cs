95a7be2486f82ebf6bbd628c133b3b85181ede92
matishen7 <ishenbaev7@gmail.com>
2025-07-29 14:08:58 -0500

    KthDistinct

--- Problems/August.cs
+++ Problems/August.cs
@@ -23,9 +23,30 @@ namespace Neetcode150.Problems
             }
             foreach (char c in s)
                 if (count[c] % 2 == 1)
-                maxOdd = Math.Max(maxOdd, count[c]);
-            else minEven = Math.Min(minEven, count[c]);
+                    maxOdd = Math.Max(maxOdd, count[c]);
+                else minEven = Math.Min(minEven, count[c]);
             return maxOdd - minEven;
+        }
+
+
+        public static string KthDistinct(string[] arr, int k)
+        {
+            var str = new Dictionary<string, int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (!str.ContainsKey(arr[i]))
+                    str.Add(arr[i], 1);
+                else str[arr[i]]++;
+            }
+            var result = new List<string>();
+            foreach (var s in str)
+            {
+                if (s.Value == 1)
+                    result.Add(s.Key);
+            }
+
+            if (result.Count < k) return "";
+            else return result[k - 1];
         }
     }
 }
